25f85fcc3f85032513ef5290a8b7a7a5080f0f5f
scottina123 <scottie.tran17@outlook.com>
2026-04-11 18:07:57 -0400

    Adjusted the tree-selection behaviors to stop throwing when a stale node is no longer present during tree refresh/virtualization

--- LegendaryExplorer/LegendaryExplorer/SharedUI/PeregrineTreeView/NodeTreeSelectionBehavior.cs
+++ LegendaryExplorer/LegendaryExplorer/SharedUI/PeregrineTreeView/NodeTreeSelectionBehavior.cs
@@ -48,60 +48,9 @@ namespace LegendaryExplorer.SharedUI.PeregrineTreeView
             foreach (var node in nodeDynasty)
             {
                 // first try the easy way
-                var newParent = currentParent.ItemContainerGenerator.ContainerFromItem(node) as TreeViewItem;
-                var index = 0;
-                VirtualizingPanel virtualizingPanel = null;
-                if (newParent == null)
+                if (!TryGetTreeViewItem(currentParent, node, out TreeViewItem newParent))
                 {
-                    // if this failed, it's probably because of virtualization, and we will have to do it the hard way.
-                    // this code is influenced by TreeViewItem.ExpandRecursive decompiled code, and the MSDN sample at http://code.msdn.microsoft.com/Changing-selection-in-a-6a6242c8/sourcecode?fileId=18862&pathId=753647475
-                    // see also the question at http://stackoverflow.com/q/183636/46635
-                    currentParent.ApplyTemplate();
-                    var itemsPresenter = (ItemsPresenter)currentParent.Template.FindName("ItemsHost", currentParent);
-                    if (itemsPresenter != null)
-                    {
-                        itemsPresenter.ApplyTemplate();
-                    }
-                    else
-                    {
-                        currentParent.UpdateLayout();
-                    }
-
-                    virtualizingPanel = GetItemsHost(currentParent) as VirtualizingPanel;
-                    CallEnsureGenerator(virtualizingPanel);
-                    index = currentParent.Items.IndexOf(node);
-                    if (index < 0)
-                    {
-                        throw new InvalidOperationException("Node '" + node + "' cannot be fount in container");
-                    }
-                    if (virtualizingPanel != null)
-                    {
-                        //This can cause an exception still (InvalidOperationException) if content generation is in progress. 
-                        //Will have to figure out how to deal with it.
-                        try
-                        {
-                            virtualizingPanel.BringIndexIntoViewPublic(index);
-                        }
-                        catch 
-                        {
-                            //This seems to be an internal exception
-                        }
-                    }
-                    newParent = currentParent.ItemContainerGenerator.ContainerFromIndex(index) as TreeViewItem;
-                    if (newParent == null)
-                    {
-                        currentParent.UpdateLayout();
-                        try
-                        {
-                            virtualizingPanel.BringIndexIntoViewPublic(index);
-                        }
-                        catch
-                        {
-                            //This seems to be an internal exception
-                            return; //?
-                        }
-                        newParent = currentParent.ItemContainerGenerator.ContainerFromIndex(index) as TreeViewItem;
-                    }
+                    return;
                 }
 
                 if (newParent == null)
@@ -121,6 +70,75 @@ namespace LegendaryExplorer.SharedUI.PeregrineTreeView
                 currentParent = newParent;
             }
         }
+
+        private static bool TryGetTreeViewItem(ItemsControl currentParent, object node, out TreeViewItem newParent)
+        {
+            newParent = currentParent.ItemContainerGenerator.ContainerFromItem(node) as TreeViewItem;
+            if (newParent != null)
+            {
+                return true;
+            }
+
+            currentParent.ApplyTemplate();
+            var itemsPresenter = (ItemsPresenter)currentParent.Template.FindName("ItemsHost", currentParent);
+            if (itemsPresenter != null)
+            {
+                itemsPresenter.ApplyTemplate();
+            }
+            else
+            {
+                currentParent.UpdateLayout();
+            }
+
+            var virtualizingPanel = GetItemsHost(currentParent) as VirtualizingPanel;
+            if (virtualizingPanel != null)
+            {
+                CallEnsureGenerator(virtualizingPanel);
+            }
+
+            int index = currentParent.Items.IndexOf(node);
+            if (index < 0)
+            {
+                Debug.WriteLine($"Skipping tree selection for node '{node}' because it is no longer present in the current container.");
+                return false;
+            }
+
+            if (virtualizingPanel != null)
+            {
+                try
+                {
+                    virtualizingPanel.BringIndexIntoViewPublic(index);
+                }
+                catch
+                {
+                    //This seems to be an internal exception
+                }
+            }
+
+            newParent = currentParent.ItemContainerGenerator.ContainerFromIndex(index) as TreeViewItem;
+            if (newParent != null)
+            {
+                return true;
+            }
+
+            currentParent.UpdateLayout();
+            if (virtualizingPanel != null)
+            {
+                try
+                {
+                    virtualizingPanel.BringIndexIntoViewPublic(index);
+                }
+                catch
+                {
+                    //This seems to be an internal exception
+                    return false;
+                }
+            }
+
+            newParent = currentParent.ItemContainerGenerator.ContainerFromIndex(index) as TreeViewItem;
+            return newParent != null;
+        }
+
         private bool _isCleanedUp;
 
         private void Cleanup()
